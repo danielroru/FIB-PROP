e18c9dc616bc04b9dae6c9b5465f42ae13452f20
Marc <marc.casellas@est.fib.upc.edu>
2018-10-16 09:20:51 +0200

    Casos d'ús

---
+++ Generador Horaris/GeneradorHoraris/src/generadorhoraris/Assignatura.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package generadorhoraris;
+
+// Identificació
+private int id;
+private String nom;
+
+// Hores de classe
+private int HoresT;
+private int HoresL;
+private int HoresP;
+
+// Número de grups
+private int nGrupsT;
+private int nGrupsL;
+private int nGrupsP;
+
+
+/**
+ *
+ * @author marccasellas
+ */
+public class Assignatura {
+    
+}
