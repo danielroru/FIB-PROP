7fafd7eea2c7ac84c4fc06478bd0b9b097c85b9d
Marc <marc.casellas@est.fib.upc.edu>
2018-10-24 18:17:30 +0200

    Aules

--- Projecte PROP/src/GeneradorHoraris/ConjuntAules.java
+++ Projecte PROP/src/GeneradorHoraris/ConjuntAules.java
@@ -3,7 +3,16 @@ package GeneradorHoraris;
 import java.util.*;
 
 public class ConjuntAules {
+
+    /* --------------------------------------------------------------------------
+        ATRIBUTS
+    -------------------------------------------------------------------------- */
+
     Set<Aula> conjuntAules = new HashSet<Aula>();
+
+    /* --------------------------------------------------------------------------
+        MÈTODES
+    -------------------------------------------------------------------------- */
 
     public void afegirAula(int id, int capacitat, Aula.TipusAula tipus) {
         Aula a = new Aula(id,capacitat,tipus);
