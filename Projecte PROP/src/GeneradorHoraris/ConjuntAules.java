0f12aae35df1273778e0b35effa669fb1b6a8638
Marc <marc.casellas@est.fib.upc.edu>
2018-10-24 19:21:29 +0200

    Conjunt d'aules

--- Projecte PROP/src/GeneradorHoraris/ConjuntAules.java
+++ Projecte PROP/src/GeneradorHoraris/ConjuntAules.java
@@ -8,15 +8,39 @@ public class ConjuntAules {
         ATRIBUTS
     -------------------------------------------------------------------------- */
 
-    Set<Aula> conjuntAules = new HashSet<Aula>();
+
+    //Set<Aula> conjuntAules = new HashSet<Aula>();
+
+    Map<String,Assignatura> conjuntAules = new HashMap<String,Assignatura>();
 
     /* --------------------------------------------------------------------------
         MÈTODES
     -------------------------------------------------------------------------- */
 
-    public void afegirAula(int id, int capacitat, Aula.TipusAula tipus) {
+    public void afegirAula() {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("ID de l'aula:");
+        int id = scanner.nextLine();
+
+        System.out.println("Capacitat de l'aula:");
+        int capacitat = scanner.nextInt();
+
+        System.out.println("Tipus de l'aula:");
+        System.out.println("    Teoria      -> 1");
+        System.out.println("    Laboratori  -> 2");
+
+        Aula.TipusAula tipus;
+        int numTipusAula = scanner.nextInt();
+
+        if (numTipusAula == 1 || numTipusAula == 2) {
+            tipus = numTipusAula == 1 ? Aula.TipusAula.TEORIA : Aula.TipusAula.LABORATORI;
+        }
+
+
         Aula a = new Aula(id,capacitat,tipus);
-        conjuntAules.add(a);
+        conjuntAules.put(id, a);
     }
 
     public void eliminarAula(Aula a) {
