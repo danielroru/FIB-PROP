7d2de4abd7a84f179fd6d809ce2f0fe868d47a91
JesusMiir <jesusmercadalmir@gmail.com>
2018-11-09 15:49:52 +0100

    CjtAssignatures

--- Projecte PROP/src/Dominio/ConjuntAssignatures.java
+++ Projecte PROP/src/Dominio/ConjuntAssignatures.java
@@ -2,38 +2,59 @@ package Dominio;
 import java.util.*;
 
 
-public class ConjuntAssignatures {
-    /*
-    private Set<Assignatura> CjtAssig;
-    private int a;
+public class CjtAssignatures {
 
-    public Assignatura[] getAssignaturesNivell(int nivell) {
-        Assignatura[] as
-        for (int i = 0; i < assignatures.length(); i++) {
-            if assignatures[i].getNivell() == nivell;
+    private Set<Assignatura> cjtAssignatures;
+    private HashMap<Integer, Set<Assignatura>> cjtNivell = new HashMap<>() ;
 
+    // Conjunt Assignatures
+
+    public CjtAssignatures() {
+        this.cjtAssignatures = new HashSet<>();
+        this.cjtNivell = new HashMap<Integer, Set<Assignatura>>();
+    }
+
+    // Consultora
+
+    public Set<Assignatura> getCjtAssignatures() {
+        return this.cjtAssignatures;
+    }
+
+    public HashMap<Integer,Set<Assignatura>> getCjtNivells() {
+        return this.cjtNivell;
+    }
+
+    public Set<Assignatura> getNivell(Integer nombre) {
+        Set<Assignatura> nivell = new HashSet<>();
+        nivell = cjtNivell.get(nombre);
+        return nivell;
+    }
+
+    // Modificadores
+
+    public void setCjtAssig(Set<Assignatura> cjtAssignatures) {
+        this.cjtAssignatures = cjtAssignatures;
+    }
+
+    public void setCjtNivells(HashMap<Integer,Set<Assignatura>> cjtNivells) {
+        this.cjtNivell = cjtNivell;
+    }
+
+
+    // Afegir Assignatura
+
+    public void addAssignatura(Assignatura assig) {
+        this.cjtAssignatures.add(assig);
+        Set<Assignatura> nivell = cjtNivell.get(assig.getNivell());
+        if (nivell == null) {
+            Set<Assignatura> aux = new HashSet<>();
+            aux.add(assig);
+            cjtNivell.put(assig.getNivell(), aux);
+        }
+        else {
+            nivell.add(assig);
+            cjtNivell.put(assig.getNivell(), nivell);
         }
     }
 
-    public ConjuntAssignatures() {
-
-    }
-
-    public afegirAssignatura(Assignatura a) {
-       // assignatures.push(a);
-    }
-
-    public eliminarAssignatura(Assignatura a) {
-        //assignatures.pop(a)
-    }
-
-    public llistarAssignatures() {
-        Scanner scanner = new Scanner(System.in);
-        for (int i = 0; i < assignatures.length(); ++i) {
-            System.out.println(assignatures[i]);
-        }
-    }
-
-    // Nivell que pugui agafar conjunts de ses assignatures interessades
-    */
 }
