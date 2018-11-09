060b28a2b7feb698818b0451d204178676ba14e3
JesusMiir <jesusmercadalmir@gmail.com>
2018-11-09 23:45:52 +0100

    CtrlDomini

--- Projecte PROP/src/Dominio/CtrlDomini.java
+++ Projecte PROP/src/Dominio/CtrlDomini.java
@@ -4,31 +4,32 @@ import java.util.*;
 public class CtrlDomini {
 
     public Set<Sessio> transfAssigSessio(ConjuntAssignatures cjtAssig) {
-        Set<Assignatura> cjtAssignatures = new HashSet<>();
         Set<Sessio> cjtSessio = new HashSet<>();
-        cjtAssignatures = cjtAssig.getConjuntAssignatures();
-        int i = 1;
-        for (Assignatura a : cjtAssignatures) {
-            Sessio sT = new Sessio();
-            sT.setNomAssig(a.getNom());
-            sT.setIdGrup(i*10);
-            sT.setDuracio(a.getnHoresT());
-            cjtSessio.add(sT);
-            for(int j = 1; j <= a.getnGrupsL(); ++j) {
-                Sessio sL = new Sessio();
-                sL.setNomAssig(a.getNom());
-                sL.setIdGrup(i*10+j);
-                sL.setDuracio(a.getnHoresT());
-                cjtSessio.add(sL);
+        for (Assignatura a : cjtAssig.getConjuntAssignatures()) {
+            for(int i = 1; i <= a.getnGrupsT(); ++i) {
+                Sessio sT = new Sessio();
+                sT.setNomAssig(a.getNom());
+                sT.setIdGrup(i * 10);
+                sT.setDuracio(a.getnHoresT());
+                sT.setTipus(Enumeracio.TipusSessio.TEORIA);
+                cjtSessio.add(sT);
+                for (int j = 1; j <= a.getnGrupsL(); ++j) {
+                    Sessio sL = new Sessio();
+                    sL.setNomAssig(a.getNom());
+                    sL.setIdGrup(i * 10 + j);
+                    sL.setDuracio(a.getnHoresL());
+                    sL.setTipus(Enumeracio.TipusSessio.LABORATORI);
+                    cjtSessio.add(sL);
+                }
+                for (int j = 1; j <= a.getnGrupsP(); ++j) {
+                    Sessio sP = new Sessio();
+                    sP.setNomAssig(a.getNom());
+                    sP.setIdGrup(i * 10 + j);
+                    sP.setDuracio(a.getnHoresP());
+                    sP.setTipus(Enumeracio.TipusSessio.PROBLEMES);
+                    cjtSessio.add(sP);
+                }
             }
-            for(int j = 1; j <= a.getnGrupsL(); ++j) {
-                Sessio sP = new Sessio();
-                sP.setNomAssig(a.getNom());
-                sP.setIdGrup(i*10+j);
-                sP.setDuracio(a.getnHoresT());
-                cjtSessio.add(sP);
-            }
-            ++i;
         }
         return cjtSessio;
     }
