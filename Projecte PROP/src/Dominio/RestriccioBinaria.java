12dc78973b29f7a12f8c261e8e4d216331f16c87
Júlia <juliaa.bl@hotmail.com>
2018-11-10 10:51:40 +0100

    Restriccio Nivell

--- Projecte PROP/src/Dominio/RestriccioBinaria.java
+++ Projecte PROP/src/Dominio/RestriccioBinaria.java
@@ -9,8 +9,15 @@ public class RestriccioBinaria {
         Assignatura assig = CtrlDomini.getPlaEstudis().getCjtAssig().getConjuntAssignatures().get(s.getNomAssig());
         Set<Assignatura> CjtNivells = CtrlDomini.getPlaEstudis().getCjtNivells().getNivell(assig.getNivell());
         for (Assignatura a : CjtNivells) {
+            //Iterar per totes les sessions d'assignatures del mateix nivell que s
             for (Sessio sessio : CtrlDomini.getSessionsByIdAssig(a.getNom())) {
-                sessionsNivell.add(sessio);
+                //Si la sessió és de teoria no es pot solapar amb les altres del mateix grup
+                if (s.getIdGrup()%10 == 0 && sessio.getIdGrup() == s.getIdGrup())
+                    sessionsNivell.add(sessio);
+                    //si la sessió és de lab no es pot solapar amb les de teoria del mateix grup ni les altres del mateix grup de lab
+                else if (s.getIdGrup()%10 != 0 &&
+                        ((sessio.getIdGrup()%10 == 0 && sessio.getIdGrup()/10 == s.getIdGrup()/10) || (sessio.getIdGrup() == s.getIdGrup())))
+                          sessionsNivell.add(sessio);
             }
         }
         return sessionsNivell;
